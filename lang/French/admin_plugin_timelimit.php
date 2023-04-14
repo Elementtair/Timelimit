5e1012c9a649cbe6eda44405bf61eb310bf75907
Elementtair <75228621+Elementtair@users.noreply.github.com>
2023-04-14 15:33:18 +0200

    initial commit

---
+++ lang/French/admin_plugin_timelimit.php
@@ -0,0 +1,14 @@
+<?php
+
+$lang_admin_plugin_timelimit = array(
+
+'Plugin title'	=>	'Timelimit - Restriction de la fonction de modification des posts',
+'Explanation 1'			=>	'Le plugin sert à installer une durée maximum, personnalisée par groupes, pour l\'edition des messages.<br />Le temps s\'exprime en secondes. 0 - désactive la restriction de temps.',
+'Explanation 2'			=>	' - Les administrateurs n\'ont pas de restrictions.<br /> - Les modérateurs n\'ont pas de restrictions dans leur zone de responsabilité.',
+'Form title'	=>	'Groupes du forum',
+'Show text button'		=>	'Sauvegarder les changements',
+'Plugin redirect' => 'Options mises à jour. Redirection …',
+'Group' => 'Groupes',
+'Time' => 'Temps',
+
+);
